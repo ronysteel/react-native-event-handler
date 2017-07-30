25bca082720d003b0d5a978e0a6b7fe52d608bdd
Hiroki Sato <m@skahack.com>
2017-07-30 14:27:30 +0900

    playground

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,6 @@
 import { AppRegistry } from 'react-native'
 import Root from './js'
+// import Playground from './js/Playground'
+
 AppRegistry.registerComponent('chatnovel', () => Root)
+// AppRegistry.registerComponent('chatnovel', () => Playground)
---
+++ js/Playground.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react'
+import { View, StatusBar } from 'react-native'
+
+import Promotion from './components/Promotion'
+import moment from 'moment'
+
+StatusBar.setHidden(false)
+StatusBar.setBarStyle('dark-content')
+
+class Playground extends React.PureComponent {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Promotion
+          products={{
+            products: {
+              'test.skahack.001': {
+                identifier: '',
+                currencyCode: '',
+                currencySymbol: '',
+                description: '',
+                downloadable: false,
+                price: 50,
+                priceString: '¥50',
+                title: '1週間',
+              }
+            }
+          }}
+          ticketCount={ 1 }
+          onTapPurchase={ () => {} }
+          onTapUseTicket={ () => {} }
+          nextRechargeDate={ moment().add(30, 'minutes').valueOf() }
+          onEndRecharge={ () => {} }
+          closeModal={ () => {} }
+        />
+      </View>
+    )
+  }
+}
+export default Playground
