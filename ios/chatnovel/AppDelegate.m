db327c434019e7e0fd416f0c3984a536b0185a21
Hiroki Sato <m@skahack.com>
2017-08-04 12:34:07 +0900

    fix rootview background color

--- ios/chatnovel/AppDelegate.m
+++ ios/chatnovel/AppDelegate.m
@@ -31,7 +31,7 @@
                                                       moduleName:@"chatnovel"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.13f green:0.13f blue:0.13f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
