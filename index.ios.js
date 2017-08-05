07b7809c875015003e4aa49c51e89993e98ae244
Hiroki Sato <m@skahack.com>
2017-08-06 00:15:30 +0900

    crashlytics

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,8 @@
 import { AppRegistry } from 'react-native'
+import { init } from './js/crashlytics'
 import Root from './js'
 // import Playground from './js/Playground'
 
+init()
 AppRegistry.registerComponent('chatnovel', () => Root)
 // AppRegistry.registerComponent('chatnovel', () => Playground)
