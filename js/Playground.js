85b24990ced728adfbfcc01ac5e66b81fd497962
Hiroki Sato <m@skahack.com>
2017-08-13 23:44:26 +0900

    fix push permission layout

--- js/Playground.js
+++ js/Playground.js
@@ -2,63 +2,19 @@
 import React from 'react'
 import { View, StatusBar } from 'react-native'
 
-import Share from 'react-native-share';
-import Promotion from './components/Promotion'
-import moment from 'moment'
+import PushPermissionPopup from './components/PushPermissionPopup'
 
 StatusBar.setHidden(false)
 StatusBar.setBarStyle('dark-content')
 
 class Playground extends React.PureComponent {
   componentDidMount() {
-    Share.isAvailable('twitter')
-      .then(() => {
-        console.log('available')
-      })
-      .catch(() => {
-        console.log('unavailable')
-      })
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Promotion
-          products={{
-            products: {
-              'co.newn.chatnovel.oneweek': {
-                identifier: '',
-                currencyCode: '',
-                currencySymbol: '',
-                description: '',
-                downloadable: false,
-                price: 900,
-                priceString: '¥900',
-                title: '1ヶ月',
-              },
-              'co.newn.chatnovel.onemonth': {
-                identifier: '',
-                currencyCode: '',
-                currencySymbol: '',
-                description: '',
-                downloadable: false,
-                price: 900,
-                priceString: '¥900',
-                title: '1ヶ月',
-              }
-            }
-          }}
-          ticketCount={ 0 }
-          remainingTweetCount={ 1 }
-          isAvailableTwitter={ false }
-          onTapPurchase={ () => {} }
-          onTapUseTicket={ () => {} }
-          onTapGetTicket={ () => {} }
-          onTapRestore={ () => {} }
-          nextRechargeDate={ moment().add(30, 'minutes').valueOf() }
-          onEndRecharge={ () => {} }
-          closeModal={ () => {} }
-        />
+        <PushPermissionPopup onPress={ () => {} } />
       </View>
     )
   }
