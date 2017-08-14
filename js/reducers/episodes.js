9b7504e5b17005473fa814803ea71a48d447897e
Hiroki Sato <m@skahack.com>
2017-08-14 18:35:45 +0900

    fix episode list

--- js/reducers/episodes.js
+++ js/reducers/episodes.js
@@ -34,18 +34,10 @@ function episodes(state: Episodes = initialStates, action: Action): Episodes {
       episode.scriptIds = scriptIds
       return Object.assign({}, state, { [episodeId]: episode })
     }
+
     case 'LOAD_EPISODE_LIST_SUCCESS': {
       const { novelId, episodes } = action
-      const es = Object.values(episodes).reduce((memo, v) => {
-        memo[v.episodeId] = Object.assign({}, state[v.episodeId] || {}, {
-          id: v.episodeId,
-          title: v.title,
-          episodeUri: v.episodeUri,
-          episodeOrder: v.episodeOrder,
-        })
-        return memo
-      }, {})
-      return Object.assign({}, state, es)
+      return { ...state, ...episodes }
     }
 
     default:
