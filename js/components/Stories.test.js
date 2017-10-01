ea003a3a3038478eb97b91413cb307db59af752f
Hiroki Sato <m@skahack.com>
2017-10-01 23:26:03 +0900

    fix a test

--- js/components/Stories.test.js
+++ js/components/Stories.test.js
@@ -3,6 +3,20 @@ import renderer from 'react-test-renderer'
 
 import Stories from './Stories'
 
+jest.mock('SectionList', () => {
+  const RealComponent = require.requireActual('SectionList')
+  const React = require('React')
+  class SectionList extends React.Component {
+    render() {
+      delete this.props.getScrollableNode
+      return React.createElement('SectionList', this.props, this.props.children)
+    }
+  }
+  SectionList.propTypes = RealComponent.propTypes
+  return SectionList
+})
+
+
 it('renders without crashing', () => {
   const component = <Stories sections={[]} onSelectContent={() => {}} />
   const rendered = renderer.create(component).toJSON()
